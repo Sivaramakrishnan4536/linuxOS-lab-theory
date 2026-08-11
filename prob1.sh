af4870a43f40bdd82f3e5c8a7ac8811bd4882ca1
Sivaramakrishnan4536 <sivaramakrishnanravi1@gmail.com>
2026-08-11 14:56:46 +0000

    Add practice shell programs and outputs

---
+++ prob1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+echo : "upadting system"
+sudo apt update
+sudo apt update -y
+echo "process completed"
---
+++ prob2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "enter the name of the tool" 
+read tool
+
+if command -v "$tool" > /dev/null 2>&1
+then
+    echo "$tool is installed"
+else
+    echo "$tool is not installed"
+fi
